1316270f108536135bb5e77da9c8bcc319a47d6d
Sander JULIEN-SAINT-AMAND <sander.julien-saint-amand@epita.fr>
2020-07-09 01:51:42 +0200

    Add write support for ATA driver

--- src/driver/ata.rs
+++ src/driver/ata.rs
@@ -1,15 +1,19 @@
-//! ATA PIO driver to communicate with drives
+//! ATA PIO driver to communicate with hard disk
+//!
 
 use x86_64::port;
-
-use crate::println;
 
 const STATUS_BUSY: u8 = 0x80;
 const STATUS_READY: u8 = 0x40;
 
 const READ_PIO: u8 = 0x20;
+const WRITE_PIO: u8 = 0x30;
+
+const COMMAND_PORT: u16 = 0x1F7;
+const DATA_PORT: u16 = 0x1F0;
 
 pub fn read_sectors(lba: usize, sectors: u8, mut dst: usize) {
+    assert!(sectors > 0);
     wait_ready();
 
     // Send bit 24 - 27 of LBA and set LBA mode
@@ -25,18 +29,43 @@ pub fn read_sectors(lba: usize, sectors: u8, mut dst: usize) {
     port::outb(0x1F5, (lba >> 16) as u8);
 
     // Read
-    port::outb(0x1F7, READ_PIO);
+    port::outb(COMMAND_PORT, READ_PIO);
     for _ in 0..sectors {
         wait_ready();
-        println!("ready to read");
 
         let buffer = dst as *mut u16;
         for i in 0..256 {
             unsafe {
-                *buffer.offset(i) = port::inw(0x1F0);
+                *buffer.offset(i) = port::inw(DATA_PORT);
             }
         }
         dst += 512;
+    }
+}
+
+pub fn write_sectors(lba: usize, sectors: u8, src: &[u8]) {
+    assert!(sectors > 0);
+    wait_ready();
+
+    // Send bit 24 - 27 of LBA and set LBA mode
+    let upper = (lba >> 24) | 0xE0;
+    port::outb(0x1F6, upper as u8);
+
+    // Send number of sectors to read
+    port::outb(0x1F2, sectors);
+
+    // Send bits 0 - 23 of LBA
+    port::outb(0x1F3, lba as u8);
+    port::outb(0x1F4, (lba >> 8) as u8);
+    port::outb(0x1F5, (lba >> 16) as u8);
+
+    // Read
+    port::outb(COMMAND_PORT, WRITE_PIO);
+    for j in 0..sectors as usize {
+        wait_ready();
+        for i in 0..256 {
+            port::outb(DATA_PORT, src[j * 512 + i])
+        }
     }
 }
 
@@ -47,8 +76,8 @@ fn wait_ready() {
 fn is_ready() -> bool {
     // Poll 400ns
     for _ in 0..4 {
-        port::inb(0x1F7);
+        port::inb(COMMAND_PORT);
     }
-    let status = port::inb(0x1F7);
+    let status = port::inb(COMMAND_PORT);
     status & STATUS_BUSY == 0 && status & STATUS_READY != 0
 }
---
+++ tests/ata_read_write.rs
@@ -0,0 +1,53 @@
+#![no_std]
+#![no_main]
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+extern crate rust_os;
+
+use core::panic::PanicInfo;
+use rust_os::*;
+use core::slice::from_raw_parts_mut;
+use rust_os::x86_64::paging::tables::EntryFlag;
+
+#[no_mangle]
+#[link_section = ".kernel_start"]
+extern "C" fn _start() -> ! {
+    init();
+
+    // Get ourselves a buffer to play around with
+    let address = memory::mmap(None, EntryFlag::Writable as u64);
+    let buffer = unsafe { from_raw_parts_mut(address as *mut u8, 4096) };
+
+    // Read 4 sectors and check if first contains bootloader
+    driver::ata::read_sectors(0, 4, address);
+    assert_eq!(buffer[510], 0x55);
+    assert_eq!(buffer[511], 0xAA);
+
+    // Now write zeros in sector 0
+    for i in 0..512 {
+        buffer[i] = 0;
+    }
+    driver::ata::write_sectors(0, 1, &buffer);
+
+    // Now write ones in buffer to prevent false positives
+    for i in 0..512 {
+        buffer[i] = 0;
+    }
+
+    // Read first sector and check if all zero
+    driver::ata::read_sectors(0, 1, address);
+    for i in 0..512 {
+        assert_eq!(buffer[i], 0);
+    }
+
+    serial_println!("ata_read_write: [OK]");
+    exit_qemu(QemuExitCode::Success)
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("ata_read_write: [KO]");
+    exit_qemu(QemuExitCode::Failure)
+}
