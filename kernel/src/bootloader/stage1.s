7e0f7269036c763ca6eb53400540fac6ccbf81e6
Sander JULIEN-SAINT-AMAND <sander.julien-saint-amand@epita.fr>
2020-08-31 20:28:56 +0200

    Supply sectors instead of actual size to stage1
    This fixes the relocation errors that appear only on CI

--- kernel/src/bootloader/stage1.s
+++ kernel/src/bootloader/stage1.s
@@ -43,10 +43,9 @@ A20Enabled:
 # Load kernel
 #=============#
 
-    lea eax, [_stage2_size]  # Add size of stage2 botloader and kernel
-    lea ebx, [_kernel_size]  #
+    lea eax, [_stage2_sectors]  # Add sectors of stage2 bootloader and kernel
+    lea ebx, [_kernel_sectors]  #
     add ebx, eax
-    shr ebx, 9               # Determine number of sectors to load
 
 readBlock:
     lea si, [dap]            # Load DAP struct
