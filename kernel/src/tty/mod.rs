adb819ba5028b1390dfdfbe205cdb7044fb8a500
Sander JULIEN-SAINT-AMAND <sander.julien-saint-amand@epita.fr>
2020-09-01 23:25:35 +0200

    Update help doc

--- kernel/src/tty/mod.rs
+++ kernel/src/tty/mod.rs
@@ -27,15 +27,19 @@ pub fn run_tty() {
         let input = readline();
 
         match input.split_whitespace().nth(0).unwrap() {
-            "help" => println!(
-                "RustOS tty v1.0\nNo other commands are supported for now."
-            ),
-            "shutdown" => exit_qemu(QemuExitCode::Success),
+            "poweroff" => exit_qemu(QemuExitCode::Success),
             "ls" => fs::ls(),
             "touch" => {
                 let data: [u8; 0] = [];
                 fs::add_file(input.split_whitespace().nth(1).unwrap(), &data, 0)
             }
+            "help" => println!(
+                "RustOS tty v1.0\n\
+                ls         list files in current directory\n\
+                touch FILE Update the access and modification times of each FILE to the current time.\n\
+                poweroff   Power off the machine\n\
+                "
+            ),
             _ => print!("Unknown command: {}", input),
         }
     }
