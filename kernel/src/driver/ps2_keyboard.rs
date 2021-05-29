fb8a2f068bcbefc88711d61cc9cc53d204669388
Sander Julien-Saint-Amand <sanderjsa@hotmail.com>
2021-05-29 14:02:09 +0200

    Add shifted char map for ps2 driver

--- kernel/src/driver/ps2_keyboard.rs
+++ kernel/src/driver/ps2_keyboard.rs
@@ -1,4 +1,5 @@
 use super::vga_driver::WRITER;
+use core::sync::atomic::{AtomicBool, Ordering};
 use core::{ptr, str};
 
 const BUFFER_SIZE: usize = 2048;
@@ -65,74 +66,38 @@ pub fn readline() -> &'static str {
 }
 
 pub fn update_stdin(code: u8) {
-    if code == 0x0E {
-        unsafe { STDIN_BUFFER.remove_char() };
-    }
-
-    if let Some(value) = parse_normal_char(code) {
-        unsafe { STDIN_BUFFER.add_char(value) };
+    match code {
+        0x0E => unsafe { STDIN_BUFFER.remove_char() },
+        0x2A | 0x36 => SHIFTED.store(true, Ordering::SeqCst),
+        0xAA | 0xB6 => SHIFTED.store(false, Ordering::SeqCst),
+        _ => {
+            if let Some(value) = parse_normal_char(code) {
+                unsafe { STDIN_BUFFER.add_char(*value) };
+            }
+        }
     }
 }
 
-fn parse_normal_char(code: u8) -> Option<char> {
-    match code {
-        0x02 => Some('1'),
-        0x03 => Some('2'),
-        0x04 => Some('3'),
-        0x05 => Some('4'),
-        0x06 => Some('5'),
-        0x07 => Some('6'),
-        0x08 => Some('7'),
-        0x09 => Some('8'),
-        0x0A => Some('9'),
-        0x0B => Some('0'),
+const UNSHIFTED_MAP: &[char] = &[
+    '?', '?', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '?', '\t', 'q', 'w', 'e',
+    'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', '?', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k',
+    'l', ';', '\'', '`', '?', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '?', '*',
+    '?', ' ',
+];
 
-        0x0C => Some('-'),
-        0x0D => Some('='),
-        0x0F => Some('\t'),
+const SHIFTED_MAP: &[char] = &[
+    '?', '?', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '?', '\t', 'Q', 'W', 'E',
+    'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n', '?', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K',
+    'L', ':', '"', '~', '?', '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '?', '*', '?',
+    ' ',
+];
 
-        0x10 => Some('q'),
-        0x11 => Some('w'),
-        0x12 => Some('e'),
-        0x13 => Some('r'),
-        0x14 => Some('t'),
-        0x15 => Some('y'),
-        0x16 => Some('u'),
-        0x17 => Some('i'),
-        0x18 => Some('o'),
-        0x19 => Some('p'),
-        0x1A => Some('['),
-        0x1B => Some(']'),
-        0x1C => Some('\n'),
+static SHIFTED: AtomicBool = AtomicBool::new(false);
 
-        0x1E => Some('a'),
-        0x1F => Some('s'),
-        0x20 => Some('d'),
-        0x21 => Some('f'),
-        0x22 => Some('g'),
-        0x23 => Some('h'),
-        0x24 => Some('j'),
-        0x25 => Some('k'),
-        0x26 => Some('l'),
-        0x27 => Some(';'),
-        0x28 => Some('\''),
-        0x29 => Some('`'),
-
-        0x2B => Some('\\'),
-        0x2C => Some('z'),
-        0x2D => Some('x'),
-        0x2E => Some('c'),
-        0x2F => Some('v'),
-        0x30 => Some('b'),
-        0x31 => Some('n'),
-        0x32 => Some('m'),
-        0x33 => Some(','),
-        0x34 => Some('.'),
-        0x35 => Some('/'),
-        0x36 => Some('*'),
-
-        0x39 => Some(' '),
-
-        _ => None,
+fn parse_normal_char(code: u8) -> Option<&'static char> {
+    if SHIFTED.load(Ordering::SeqCst) {
+        SHIFTED_MAP.get(code as usize)
+    } else {
+        UNSHIFTED_MAP.get(code as usize)
     }
 }
