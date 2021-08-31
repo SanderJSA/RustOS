5242e429453cde6f83171cae894539f891889118
Sander Julien-Saint-Amand <sanderjsa@hotmail.com>
2021-08-31 16:17:15 +0200

    Make Gdt more functional and make static in load

--- kernel/src/arch/x86_64/gdt.rs
+++ kernel/src/arch/x86_64/gdt.rs
@@ -1,38 +1,47 @@
 use core::mem;
 
-const KERNEL_RING: u8 = 0;
-const USERLAND_RING: u8 = 3;
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
-const MAX_ENTRIES: usize = 64;
-static mut GDT: GDT = GDT::new();
+
+const KERNEL_RING: u8 = 0;
+#[allow(unused)]
+const USERLAND_RING: u8 = 3;
+
+const KERNEL_CODE_OFF: usize = 1;
+const KERNEL_DATA_OFF: usize = 2;
+const USER_CODE_OFF: usize = 3;
+const USER_DATA_OFF: usize = 4;
+
+const MAX_ENTRIES: usize = 8;
 
 pub fn init() {
+    /* TODO: Add TSS
+    static mut TSS: TaskStateSegment = TaskStateSegment::new();
+            TSS.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
+                const STACK_SIZE: usize = 4096;
+                static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+
+                let stack_start = VirtAddr::from_ptr(&STACK);
+                stack_start + STACK_SIZE
+            };
+    let tss_selector = Gdt.add_entry(Descriptor::tss_segment(&TSS));
+    load_tss(tss_selector);
+    */
+
+    let gdt = Gdt::new()
+        .insert(KERNEL_CODE_OFF, GdtEntry::new(0, true, KERNEL_RING))
+        .insert(KERNEL_DATA_OFF, GdtEntry::new(0, false, KERNEL_RING))
+        .insert(USER_CODE_OFF, GdtEntry::new(0, true, USERLAND_RING))
+        .insert(USER_DATA_OFF, GdtEntry::new(0, false, USERLAND_RING));
+
     unsafe {
-        /* TODO: Add TSS
-        static mut TSS: TaskStateSegment = TaskStateSegment::new();
-                TSS.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
-                    const STACK_SIZE: usize = 4096;
-                    static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
-
-                    let stack_start = VirtAddr::from_ptr(&STACK);
-                    stack_start + STACK_SIZE
-                };
-        let tss_selector = GDT.add_entry(Descriptor::tss_segment(&TSS));
-        load_tss(tss_selector);
-        */
-
-        let kernel_code_seg = GDT.add_entry(GDTEntry::new(0, 0xFFFFF, true, KERNEL_RING));
-        let kernel_data_seg = GDT.add_entry(GDTEntry::new(0, 0xFFFFF, false, KERNEL_RING));
-
-        let user_code_seg = GDT.add_entry(GDTEntry::new(0, 0xFFFFF, true, KERNEL_RING));
-        let user_data_seg = GDT.add_entry(GDTEntry::new(0, 0xFFFFF, false, KERNEL_RING));
-
-        GDT.load(kernel_data_seg, kernel_code_seg);
+        gdt.load();
     }
 }
 
+#[allow(unused)]
 const ACCESSED: u8 = 1;
 const WRITEABLE: u8 = 1 << 1;
+#[allow(unused)]
 const EXPAND_DOWN: u8 = 1 << 2;
 const EXECUTABLE: u8 = 1 << 3;
 const DESC_TYPE: u8 = 1 << 4;
@@ -40,12 +49,13 @@ const PRIVILEGE_SHIFT: u8 = 5;
 const SEG_PRESENT: u8 = 1 << 7;
 const AVL: u8 = 1 << 4;
 const CS_SIZE: u8 = 1 << 5;
+#[allow(unused)]
 const DEFAULT_OPERATION_SIZE: u8 = 1 << 6;
 const GRANULARITY: u8 = 1 << 7;
 
 #[derive(Default)]
 #[repr(C)]
-struct GDTEntry {
+struct GdtEntry {
     seg_lim_low: u16,
     base_addr_low: u16,
     base_addr_mid: u8,
@@ -63,61 +73,66 @@ struct GDTEntry {
     /// default_operation_size : 1
     /// granularity : 1
     flags: u8,
-    base_addr_high: u16,
+    base_addr_high: u8,
 }
 
-impl GDTEntry {
-    pub const fn new(base_addr: u32, limit: u32, executable: bool, ring: u8) -> GDTEntry {
+impl GdtEntry {
+    pub const fn new(base_addr: u32, executable: bool, ring: u8) -> GdtEntry {
+        let limit = 0xFFFFF;
         let (executable, long_code_seg) = match executable {
             true => (EXECUTABLE, CS_SIZE),
             false => (0, 0),
         };
-        GDTEntry {
+        GdtEntry {
             seg_lim_low: limit as u16,
             base_addr_low: base_addr as u16,
             base_addr_mid: (base_addr >> 16) as u8,
             access: WRITEABLE | DESC_TYPE | SEG_PRESENT | executable | (ring << PRIVILEGE_SHIFT),
             flags: ((limit >> 16) & 0xF) as u8 | AVL | CS_SIZE | GRANULARITY | long_code_seg,
-            base_addr_high: (base_addr >> 24) as u16,
+            base_addr_high: (base_addr >> 24) as u8,
         }
     }
 }
 
 #[repr(C)]
-struct GDT {
+struct Gdt {
     limit: u16,
-    base: [GDTEntry; MAX_ENTRIES],
-    next_entry: usize,
+    base: [GdtEntry; MAX_ENTRIES],
 }
 
-impl GDT {
-    pub const fn new() -> GDT {
-        GDT {
-            limit: mem::size_of::<[GDTEntry; MAX_ENTRIES]>() as u16,
-            base: [GDTEntry::default(); MAX_ENTRIES],
-            next_entry: 1, // 0th entry is the NULL Entry
+impl Gdt {
+    pub const fn new() -> Gdt {
+        let entries;
+        // SAFETY: A NULL entry is a valid entry and GdtEntry is 64 bits wide
+        unsafe {
+            entries =
+                mem::transmute::<[u64; MAX_ENTRIES], [GdtEntry; MAX_ENTRIES]>([0u64; MAX_ENTRIES]);
+        };
+
+        Gdt {
+            limit: mem::size_of::<[GdtEntry; MAX_ENTRIES]>() as u16,
+            base: entries,
         }
     }
 
-    pub fn add_entry(&mut self, entry: GDTEntry) -> usize {
-        let offset = self.next_entry;
-        self.base[offset] = entry;
-        self.next_entry += 1;
-        offset
+    pub fn insert(mut self, index: usize, entry: GdtEntry) -> Gdt {
+        self.base[index] = entry;
+        self
     }
 
-    /// Loads the GDT and reloads segments
-    /// GDT must be valid and given segments need to point to correct entries
-    pub unsafe fn load(&'static self, data_seg: usize, code_seg: usize) {
-        asm!("lgdt [{}]", in(reg) self);
+    /// Loads the Gdt and reloads segments
+    /// Gdt must be valid
+    pub unsafe fn load(self) {
+        static mut GDT: Option<Gdt> = None;
+        GDT = Some(self);
+
+        asm!("lgdt [{}]", in(reg) GDT.as_ref().unwrap());
         asm!("mov ds, ax",
              "mov ss, ax",
              "mov es, ax",
              "mov fs, ax",
              "mov gs, ax",
-             in("ax") data_seg);
-        asm!("jmp {}, 1:",
-             "1:",
-             in(reg) code_seg);
+             in("ax") KERNEL_DATA_OFF);
+        asm!("ljmp {}, 1f", "1:", const KERNEL_CODE_OFF);
     }
 }
